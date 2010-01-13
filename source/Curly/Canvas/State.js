167d4df920ea703fbfbd38db805f09d248b4d0f2
martin@curlybracket.de <martin@curlybracket.de@2045ddca-f887-11de-a7af-57dd9f136cd4>
2010-01-13 21:22:30 +0000

    Changed some doc comments

--- source/Curly/Canvas/State.js
+++ source/Curly/Canvas/State.js
@@ -1,38 +1,162 @@
 /**
  * @class Curly.Canvas.State
  */
+/**
+ * @constructor
+ * @param Object State configurations
+ */
 Curly.Canvas.State=function(config) {
 	Curly.extend(this, Curly.Canvas.State.DEFAULTS, config||{});
 };
+/**
+ * @final
+ * @type Array
+ */
 Curly.Canvas.State.IDENTITY_MATRIX=[1,0,0,1,0,0];
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.OP_SOURCE_OVER='source-over';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.OP_SOURCE_IN='source-in';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.OP_SOURCE_OUT='source-out';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.OP_SOURCE_atop='source-atop';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.OP_DESTINATION_OVER='destination-over';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.OP_DESTINATION_IN='destination-in';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.OP_DESTINATION_OUT='destination-out';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.OP_DESTINATION_atop='destination-atop';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.OP_LIGHTER='lighter';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.OP_COPY='copy';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.OP_XOR='xor';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.CAP_BUTT='butt';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.CAP_ROUND='round';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.CAP_SQUARE='square';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.JOIN_ROUND='round';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.JOIN_BEVEL='bevel';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.JOIN_MITER='miter';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.ALIGN_START='start';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.ALIGN_END='end';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.ALIGN_LEFT='left';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.ALIGN_RIGHT='right';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.ALIGN_CENTER='center';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.BASELINE_TOP='top';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.BASELINE_HANGING='hanging';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.BASELINE_MIDDLE='middle';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.BASELINE_ALPHABETIC='alphabetic';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.BASELINE_IDEOGRAPHIC='ideographic';
+/**
+ * @final
+ * @type String
+ */
 Curly.Canvas.State.BASELINE_BOTTOM='bottom';
+/**
+ * @final
+ * @type Object Default state configurations
+ */
 Curly.Canvas.State.DEFAULTS={
 	scaleX:				1.0,
 	scaleY:				1.0,
@@ -56,5 +180,13 @@ Curly.Canvas.State.DEFAULTS={
 	textAlign:			Curly.Canvas.State.ALIGN_START,
 	textBaseline:		Curly.Canvas.State.BASELINE_ALPHABETIC
 };
+/**
+ * @final
+ * @type String Constant value for a totally transparent color
+ */
 Curly.Canvas.State.COLOR_TRANSPARENT='rgba(0,0,0,0)';
+/**
+ * @final
+ * @type String Constant value for a totally transparent color
+ */
 Curly.Transparent='rgba(0,0,0,0)';
